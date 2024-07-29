5a623e6909bdd994d700c602b457eb378efc3acd
EricDuarteDosSantos <duarteeric898@gmail.com>
2024-07-29 17:31:01 -0300

    494498

---
+++ Controllers/AgendamentoController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using FitHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitHub.Controllers
+{
+    public class AgendamentoController : Controller
+    {
+        private readonly ILogger<AgendamentoController> _logger;
+
+        public AgendamentoController(ILogger<AgendamentoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AgendamentoUsuario()
+        {
+            return View();
+        }
+        public IActionResult AgendamentoCadastro()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using FitHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitHub.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ILogger<UsuariosController> _logger;
+
+        public UsuariosController(ILogger<UsuariosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
